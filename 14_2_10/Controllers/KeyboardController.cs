23d812d6e0cf608c318bada7c8959a683186fe02
DmitryT24 <t.tdn@yandex.ru>
2024-05-13 14:39:00 +0300

    Добавьте файлы проекта.

---
+++ 14_2_10/Controllers/KeyboardController.cs
@@ -0,0 +1,73 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Controllers
+{
+
+    public class KeyboardController
+    {
+        List<Contact> _phoneBook;
+        public KeyboardController(List<Contact> phoneBook)
+        {
+            _phoneBook = phoneBook;
+        }
+
+        public void ButtonPhoneBook()
+        {
+            Console.Write("Введите номер страницы:");
+            while (true)
+            {
+
+                try
+                {// Читаем введенный с консоли символ
+
+                    var input = Console.ReadKey().KeyChar;
+                    Console.Clear();
+                    // проверяем, число ли это
+                    var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+                    // если не соответствует критериям - показываем ошибку
+                    if (!parsed || pageNumber < 1 || pageNumber > 3)
+                    {
+                        throw new MyException("Упс! Вы ввели не корректно номер страницы! \nПожалуйста, повторите ввод!" +
+                            "\nДля продолжения введите номер страницы(не более 3х):");
+                    }
+                    // если соответствует - запускаем вывод
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"Номер страницы: {pageNumber} из  3");
+                        // пропускаем нужное количество элементов и берем 2 для показа на странице
+                        var pageContent = _phoneBook.Skip((pageNumber - 1) * 2).Take(2);
+                        Console.WriteLine();
+
+                        // выводим результат
+                        // сортировка Имя , Фамилия
+                        var sortContact = from contact in pageContent
+                                                             .OrderBy(s => s.Name)
+                                                             .ThenBy(s => s.LastName)
+                                          select contact;
+
+                        foreach (var entry in sortContact)
+                            Console.WriteLine(entry.Name + "  " + entry.LastName + ": " + entry.PhoneNumber + " - " + entry.Email);
+
+                        Console.WriteLine();
+                        Console.Write("Для продолжения введите номер страницы:");
+                    }
+                }
+                catch (Exception ex) when (ex is MyException)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ 14_2_10/MyPhoneBooks.cs
@@ -0,0 +1,33 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneBook.Controllers;
+
+namespace PhoneBook
+{
+    internal class MyPhoneBooks
+    {
+        List<Contact> phoneBook = new List<Contact>();
+        KeyboardController keyboard;
+        public MyPhoneBooks()
+        {
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+            keyboard = new KeyboardController(phoneBook);
+        }
+        public void ClickedPhoneBooks()
+        {
+            keyboard.ButtonPhoneBook();
+
+        }
+    }
+}
